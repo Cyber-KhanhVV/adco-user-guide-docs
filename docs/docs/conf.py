6de94718deebbed0ea296d4f340919af43b389d8
Vũ Văn Khánh <103799964+Cyber-KhanhVV@users.noreply.github.com>
2025-05-20 08:53:50 +0700

    Delete docs/docs directory

--- docs/docs/conf.py
+++
@@ -1,8 +0,0 @@
-project = 'My ERP Docs'
-author = 'Your Name'
-release = '1.0'
-
-extensions = ['myst_parser']
-source_suffix = ['.rst', '.md']
-master_doc = 'index'
-html_theme = 'alabaster'
--- docs/docs/introduction.rst
+++
@@ -1,4 +0,0 @@
-Giới thiệu
-==========
-
-Đây là phần mềm ERP tuyệt vời dành cho doanh nghiệp của bạn!
