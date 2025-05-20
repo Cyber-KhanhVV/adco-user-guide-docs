67b19700f332bfbe97441a54aa6b6de146c52450
Vũ Văn Khánh <103799964+Cyber-KhanhVV@users.noreply.github.com>
2025-05-20 09:50:09 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -6,3 +6,7 @@ extensions = ['myst_parser']
 source_suffix = ['.rst', '.md']
 master_doc = 'index'
 html_theme = 'alabaster'
+html_static_path = ['_static']  # Đảm bảo dòng này có
+
+def setup(app):
+    app.add_css_file('custom.css')
